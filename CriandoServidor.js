0a9b6a2fdf980a0f795b15f2b47cf6cd58cd9dde
Luis Eduardo Ferreira de Faria <luiseduardo250305@gmail.com>
2023-05-01 11:39:06 -0300

    Respondendo requisições com base na url recuperada

--- CriandoServidor.js
+++ CriandoServidor.js
@@ -1,9 +1,24 @@
 //Forma de criar um servidor utilizando node js
 
-var http = require ('http')
+var http = require('http')
 
-var server = http.createServer(function(req, res){
-    res.end("<html><body>Hello Word</body></html>")
+var server = http.createServer(function (req, res) {
+
+    var categoria = req.url
+
+    if (categoria == "/moda") {
+        res.end("<html><body>Página de moda</body></html>")
+    }
+    else if (categoria == "/tecnologia") {
+        res.end("<html><body>Tecnologia</body></html>")
+    }
+    else if (categoria == "/beleza") {
+        res.end("<html><body>Beleza</body></html>")
+    }
+    else {
+        res.end("<html><body>Hello Word</body></html>")
+    }
+
 })
 
 //Define a porta que sera executada no localhost
