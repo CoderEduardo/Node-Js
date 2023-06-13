3c9a5ae4afbbf55da829633eff3283b4cf51376d
Luis Eduardo Ferreira de Faria <luiseduardo250305@gmail.com>
2023-06-13 19:00:42 -0300

    Executando arquivos usando o node

---
+++ ExecutandoScripts/script.js
@@ -0,0 +1,13 @@
+/*Para executar scripts usando o node, precisamos acessar a pasta desejada pelo terminal, e na pasta em que está o arquivo precisamos usar o 
+node aruivo.js para rodar o arquivo desejado*/
+
+
+let a = 20
+let b = 30
+let resultado = soma(a,b)
+
+function soma(a,b){
+    return a+b
+}
+
+console.log(resultado)
