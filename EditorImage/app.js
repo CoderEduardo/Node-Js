bd57361bb71ea6773141aad5b258fa885d80c127
Luis Eduardo Ferreira de Faria <luiseduardo250305@gmail.com>
2023-06-14 20:10:05 -0300

    Comprimindo o tamanho da imagem com node

--- EditorImage/app.js
+++ EditorImage/app.js
@@ -1,21 +1,51 @@
 const sharp = require("sharp")
+const compress_images = require("compress-images")
+const fs = require('fs')
+const path = require("path")
 
 let diretorio = process.argv[2]
 let width = Number(process.argv[3])
 
 console.log(diretorio, width)
 
-function redimensionar(diretorio, width) {
+function redimensionar(diretorio, saidaImage ,width) {
 
-    sharp(diretorio).resize({ width: width }).toFile("./temp/outuput_risize.jpg", (error) => {
+    sharp(diretorio).resize({ width: width }).toFile(saidaImage, (error) => {
         if (error) {
             console.log(error)
         } else {
             console.log("Imagem redimensionada com sucesso")
+            comprimirImage(saidaImage,"./comprimida/")
         }
     })
+}
+
+function comprimirImage(diretorioImage,saidaImage){
+
+    compress_images(diretorioImage, saidaImage, { compress_force: false, statistic: true, autoupdate: true }, false,
+        { jpg: { engine: "mozjpeg", command: ["-quality", "60"] } },
+        { png: { engine: "pngquant", command: ["--quality=20-50", "-o"] } },
+        { svg: { engine: "svgo", command: "--multipass" } },
+        { gif: { engine: "gifsicle", command: ["--colors", "64", "--use-col=web"] } },
+function (error, completed, statistic) {
+console.log("-------------");
+console.log(error);
+console.log(completed);
+console.log(statistic);
+console.log("-------------");
+
+fs.unlink(diretorioImage, (error) =>{
+    if(error){
+        console.log(error)
+    }else{
+        console.log(diretorioImage + " Apagado")
+    }
+})
+
+}
+);
 
 }
 
-redimensionar(diretorio,width)
+redimensionar(diretorio,"./temp/output_resize.jpg",width)
 
