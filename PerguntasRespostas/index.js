8f36a077a258cc0922f9d14e15aa4246b83cf1a5
Luis Eduardo Ferreira de Faria <luiseduardo250305@gmail.com>
2023-09-05 15:54:59 -0300

    modified:   PerguntasRespostas/index.js 	new file:   PerguntasRespostas/public/css/bootstrap-grid.css 	new file:   PerguntasRespostas/public/css/bootstrap-grid.css.map 	new file:   PerguntasRespostas/public/css/bootstrap-grid.min.css 	new file:   PerguntasRespostas/public/css/bootstrap-grid.min.css.map 	new file:   PerguntasRespostas/public/css/bootstrap-reboot.css 	new file:   PerguntasRespostas/public/css/bootstrap-reboot.css.map 	new file:   PerguntasRespostas/public/css/bootstrap-reboot.min.css 	new file:   PerguntasRespostas/public/css/bootstrap-reboot.min.css.map 	new file:   PerguntasRespostas/public/css/bootstrap.css 	new file:   PerguntasRespostas/public/css/bootstrap.css.map 	new file:   PerguntasRespostas/public/css/bootstrap.min.css 	new file:   PerguntasRespostas/public/css/bootstrap.min.css.map 	new file:   PerguntasRespostas/public/js/bootstrap.bundle.js 	new file:   PerguntasRespostas/public/js/bootstrap.bundle.js.map 	new file:   PerguntasRespostas/public/js/bootstrap.bundle.min.js 	new file:   PerguntasRespostas/public/js/bootstrap.bundle.min.js.map 	new file:   PerguntasRespostas/public/js/bootstrap.js 	new file:   PerguntasRespostas/public/js/bootstrap.js.map 	new file:   PerguntasRespostas/public/js/bootstrap.min.js 	new file:   PerguntasRespostas/public/js/bootstrap.min.js.map 	new file:   PerguntasRespostas/public/style.css

--- PerguntasRespostas/index.js
+++ PerguntasRespostas/index.js
@@ -3,22 +3,13 @@ const app = express()
 const PORTA = 8080
 
 app.set("view engine", "ejs")        //Dizendo para o express usar o ejs com engine
+app.use(express.static("public"))
 
 app.get("/", (req, res) => {
-    let nome = "Luis Eduardo"
-    let linguagem = "JS"
-    let exibirMsg = true
-    let produtos = [
-        { nome: "Doritos", preco: 5 },
-        { nome: "Cola-cola", preco: 7 },
-        { nome: "Leite", preco: 3 }
-    ]
+      
 
     res.render("index", {
-        nome: nome,
-        linguagem: linguagem,
-        msg: exibirMsg,
-        produtos:produtos
+       
     })
 })
 
