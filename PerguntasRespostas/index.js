5b79a687c35b458e67952895111271d604ab1b0c
Luis Eduardo Ferreira de Faria <luiseduardo250305@gmail.com>
2023-09-06 13:29:55 -0300

    Recebendo dados com o bodyParser

--- PerguntasRespostas/index.js
+++ PerguntasRespostas/index.js
@@ -2,27 +2,35 @@
 const express = require("express")
 const app = express()
 const PORTA = 8080
+const bodyParser = require("body-parser")
 app.set("view engine", "ejs")        //Dizendo para o express usar o ejs com engine
 app.use(express.static("public"))    //Dizendo para o express usar a pasta public como caminho para arquivos estáticos
+app.use(bodyParser.urlencoded({extended:false})) //Permite que os dados recebidos sejam decodifcados pelo bodyParser
+app.use(bodyParser.json())  //Permite a leitura de dados enviados via json
 /*********************************************************************************************************************/
 
 /*ROTAS*/
 app.get("/", (req, res) => {
-    let titulo = "Guia de perguntas"
+    let cabecalho = "Guia de perguntas"
     res.render("index", {
-       titulo:titulo
+        cabecalho:cabecalho
     })
 })
 
 app.get("/perguntar",(req,res)=>{
-    let titulo = "Faça sua pegunta"
+    let cabecalho = "Faça sua pegunta"
     res.render("perguntar",{
-        titulo:titulo
+        cabecalho:cabecalho
     })
 })
 
 app.post("/salvarpergunta",(req,res)=>{
-    res.send("Formulário recebido")
+
+    let titulo = req.body.titulo
+    let descricao = req.body.descricao
+
+    res.send(`Título: ${titulo}. <br> Descrição: ${descricao}`)
+    
 })
 /*********************************************************************************************************************/
 
