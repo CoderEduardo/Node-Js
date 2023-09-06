17852b982f63601a556bd469a7350eb00a2b6974
Luis Eduardo Ferreira de Faria <luiseduardo250305@gmail.com>
2023-09-06 13:01:24 -0300

    Adicionando o include na aplicação

--- PerguntasRespostas/index.js
+++ PerguntasRespostas/index.js
@@ -1,21 +1,30 @@
+/*CONFIGURAÇÃO DO SISTEMA*/
 const express = require("express")
 const app = express()
 const PORTA = 8080
-
 app.set("view engine", "ejs")        //Dizendo para o express usar o ejs com engine
-app.use(express.static("public"))
+app.use(express.static("public"))    //Dizendo para o express usar a pasta public como caminho para arquivos estáticos
+/*********************************************************************************************************************/
 
+/*ROTAS*/
 app.get("/", (req, res) => {
-      
-
+    let titulo = "Guia de perguntas"
     res.render("index", {
-       
+       titulo:titulo
     })
 })
 
 app.get("/perguntar",(req,res)=>{
-    res.render("perguntar")
+    let titulo = "Faça sua pegunta"
+    res.render("perguntar",{
+        titulo:titulo
+    })
 })
+
+app.post("/salvarpergunta",(req,res)=>{
+    res.send("Formulário recebido")
+})
+/*********************************************************************************************************************/
 
 app.listen(PORTA, () => {
     console.log(`O servidor está rodando na porta ${PORTA}`)
