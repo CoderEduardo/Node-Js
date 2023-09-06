3a3f56f8d1f5ba8e5458aeca797a5249a4a0e365
Luis Eduardo Ferreira de Faria <luiseduardo250305@gmail.com>
2023-09-06 14:40:52 -0300

    Criando a tabela de perguntas com o sequelize

--- PerguntasRespostas/index.js
+++ PerguntasRespostas/index.js
@@ -4,6 +4,7 @@ const app = express()
 const PORTA = 8080
 const connection = require("./database/database")
 connection.authenticate().then(()=> console.log("Conexão feita com o bando de dados")).catch(erro => console.log(`Ocorreu um erro ${erro}`))
+const perguntaModel = require('./database/Pergunta')
 const bodyParser = require("body-parser")
 app.set("view engine", "ejs")        //Dizendo para o express usar o ejs com engine
 app.use(express.static("public"))    //Dizendo para o express usar a pasta public como caminho para arquivos estáticos
