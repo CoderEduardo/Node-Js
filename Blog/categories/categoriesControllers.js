6c56616ecf7029d4033d9fcbe73cef1799918715
Luis Eduardo Ferreira de Faria <luiseduardo250305@gmail.com>
2023-09-09 18:14:03 -0300

    Criando a rota para atualizar as categorias

--- Blog/categories/categoriesControllers.js
+++ Blog/categories/categoriesControllers.js
@@ -51,6 +51,17 @@ router.get("/admin/categories/edit/:id", (req, res) => {
             res.redirect("/admin/categories")
         }
     }).catch(() => {
+        res.redirect("/admin/categories")   
+    })
+})
+
+router.post("/categories/update",(req,res)=>{
+    let id = req.body.id
+    let title = req.body.title
+
+    Category.update({title:title,slug:slugify(title)},{
+        where:{id:id}
+    }).then(()=>{
         res.redirect("/admin/categories")
     })
 })
