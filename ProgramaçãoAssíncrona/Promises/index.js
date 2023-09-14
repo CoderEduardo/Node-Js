c798a060c6a0dcec8a018def7c4f266c3d0b4971
Luis Eduardo Ferreira de Faria <luiseduardo250305@gmail.com>
2023-09-14 17:08:36 -0300

    Passando parâmetros para as Promises

--- ProgramaçãoAssíncrona/Promises/index.js
+++ ProgramaçãoAssíncrona/Promises/index.js
@@ -2,21 +2,26 @@ function enviarEmail(corpo, para) {
     return new Promise((resolve, reject) => {
 
         setTimeout(() => {
-            let deuErro = false
+            let deuErro = true
             if (!deuErro) {
-                resolve()
+                resolve({mensagem1:"Deu certo",mensagem2:"Deu bom"}) //Podemos passar parâmetros para enviar para o then
             } else {
-                reject()
+                reject("Deu errado aqui parceiro")  //Podemos passar parâmetros para enviar para o catch
             }
         }, 4000);
 
     })
 }
 
-enviarEmail("Olá mundo","Luis Eduardo").then(()=>{
-    console.log("Processo feito com sucesso")
-}).catch((erro)=>{
-    console.log(`Ocorreu um erro ${erro}`)
+enviarEmail("Olá mundo","Luis Eduardo").then(dados=>{
+    console.log(dados.mensagem2)
+}).catch(erro=>{
+    console.log(erro)
 })
 
-/*A Promise executa o then quando é retornado o resolve(), e executa o catch quando é retornado o reject()*/
+/*
+A Promise executa o then quando é retornado o resolve(), e executa o catch quando é retornado o reject()
+
+Ao passar parâmetros para uma Promise, os parâmetros passados pelo resolve serão referentes ao then, e os parâmetros passados pelo reject serão referentes
+ao catch
+*/
