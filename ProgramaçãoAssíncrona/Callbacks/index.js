25b92da24c3dd5a776bdc6f54e56e2a87308f4d2
Luis Eduardo Ferreira de Faria <luiseduardo250305@gmail.com>
2023-09-14 14:15:34 -0300

    Trabalhando com funções de callback

--- ProgramaçãoAssíncrona/Callbacks/index.js
+++ ProgramaçãoAssíncrona/Callbacks/index.js
@@ -1,4 +1,4 @@
-function enviarEmail(corpo,para){
+function enviarEmail(corpo,para,callback){
     setTimeout(()=>{
         console.log(`
             Para: ${para}
@@ -7,10 +7,15 @@ function enviarEmail(corpo,para){
             -------------
             De Luis Eduardo
         `)
+        callback()
     },5000)
 }
 
 console.log("Email sendo enviado")
-enviarEmail("Bom dia, seja bem-vindo","Marcelo")
+enviarEmail("Bom dia, seja bem-vindo","Marcelo",()=>{
+    console.log("Função de callback chamada")
+})
 console.log("O email logo vai chegar para você")
 //Esse é um exemplo de programação assíncrona, ou seja, não segue uma linha linear, esperando um ser executado para somente depois executar outra coisa
+
+//Uma função de callback só é executada depois de uma outra função ser executada, ela espera a reposta de uma para depois ela rodar
