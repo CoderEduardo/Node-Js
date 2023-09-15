38733e273cd465abae41276726db68f7dc04dce6
Luis Eduardo Ferreira de Faria <luiseduardo250305@gmail.com>
2023-09-15 00:45:41 -0300

    Passando os valores do objeto por parâmetro do construtor

--- POO/Objetos/index.js
+++ POO/Objetos/index.js
@@ -1,13 +1,13 @@
 class Filme {
 
-    constructor() {
+    constructor(titulo,ano,genero,diretor,duracao) {
         //Defindo os atributos de uma classe
-        this.titulo = ""
-        this.ano = 0
-        this.genero = ""
-        this.diretor = ""
+        this.titulo = titulo 
+        this.ano = ano
+        this.genero = genero 
+        this.diretor = diretor 
         this.atores = []
-        this.duracao = 0
+        this.duracao = duracao
 
     }
 
@@ -35,9 +35,11 @@ class Filme {
 
 let vingadores = new Filme()    //Criando um objeto
 let starWars = new Filme()
+let batman = new Filme("Batman",2000,"Ação","Zé da manga","2 horas e 50 minutos")   //Passando os valores por parâmetros pelo construtor
 
 vingadores.Reproduzir() //Acessando métodos
 starWars.Fechar()
 vingadores.titulo = "Vingadores"    //Definindo valores aos atributos
 vingadores.genero = "Ação"
 console.log(`Nome: ${vingadores.titulo}, gênero: ${vingadores.genero}`) //Acessando atributos
+console.log(`Filme: ${batman.titulo}, Ano: ${batman.ano}, Gênero: ${batman.genero}, Diretor: ${batman.diretor}, Duração: ${batman.duracao}`)
