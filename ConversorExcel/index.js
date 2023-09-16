f2d2897e94ef11ba7b2335646d0425e1d824121e
Luis Eduardo Ferreira de Faria <luiseduardo250305@gmail.com>
2023-09-16 19:29:17 -0300

    Recebendo valores de arquivos json e alterando

--- ConversorExcel/index.js
+++ ConversorExcel/index.js
@@ -13,8 +13,31 @@ const fs = require("fs")
 })*/
 
 //Essa é uma forma de escrever em um arquivo, sendo o conteúdo passado como o segundo parâmetro
-fs.writeFile("./teste.txt", "Novo conteúdo Novamente", (erro) => {
+/*fs.writeFile("./teste.txt", "Novo conteúdo Novamente", (erro) => {
     if(erro){
         console.log(`Ocorreu um erro ${erro}`)
     }
-})
+})*/
+
+/*Conseguimos passar valores para o nosso arquivo, para adicionar um atributo novo no json, precisamos converter a string recebida em um json, e para
+isso usamos o JSON.parse(), depois de atribuit os respectivos valores, precisamos ler o arquivo como uma string, e para isso usamos o JSON.stringify()*/
+function modificarUsuario(nome,curso,categoria){
+    fs.readFile("./usuario.json",{encoding:"utf-8"},(erro,dados)=>{
+        if(erro){
+            console.log(`Ocorreu um erro ${erro}`)
+        }else{
+            /*Essa é uma forma de let um arquivo json e modificar os seus atributos*/
+            let conteudo = JSON.parse(dados)
+            conteudo.nome = nome 
+            conteudo.curso = curso
+            conteudo.categoria = categoria
+            fs.writeFile("./usuario.json",JSON.stringify(conteudo),erro=>{
+                if(erro){
+                    console.log(`Ocorreu um erro: ${erro}`)
+                }
+            })
+        }
+    })
+}
+
+modificarUsuario("Joaquin","Enfermagem","Area da saúde")
