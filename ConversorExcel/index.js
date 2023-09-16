63cd8b5ed1739f95332c89061d1b77091a840ff1
Luis Eduardo Ferreira de Faria <luiseduardo250305@gmail.com>
2023-09-16 16:37:05 -0300

    Escrevendo em um arquivo

--- ConversorExcel/index.js
+++ ConversorExcel/index.js
@@ -3,11 +3,18 @@ const fs = require("fs")
 
 //O primeiro parâmetro passado, nos remete ao diretorio, o segundo ao tipo da leitura do arquivo, e no terceiro recebemos o erro como primeiro parâmetro
 //e os dados como segundo
-fs.readFile("./teste.txt",{encoding:"utf-8"},(erro,dados)=>{
+/*fs.readFile("./teste.txt",{encoding:"utf-8"},(erro,dados)=>{
     //verificamos o erro
     if(erro){
         console.log("Ocorreu um erro")
     }else{
         console.log(dados)
+    }
+})*/
+
+//Essa é uma forma de escrever em um arquivo, sendo o conteúdo passado como o segundo parâmetro
+fs.writeFile("./teste.txt", "Novo conteúdo Novamente", (erro) => {
+    if(erro){
+        console.log(`Ocorreu um erro ${erro}`)
     }
 })
