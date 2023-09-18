cec2ff1f9166afcd851f830fdd2cfef68cf7c975
Luis Eduardo Ferreira de Faria <luiseduardo250305@gmail.com>
2023-09-18 16:55:12 -0300

    Cadastrando novos jogos com o sequelize

--- CriandoApi-Rest/GameController/gameController.js
+++ CriandoApi-Rest/GameController/gameController.js
@@ -1,4 +1,33 @@
 const express = require("express")
 const router = express.Router()
+const Game = require("./Game")
+
+router.get("/games",(req,res)=>{
+    Game.findAll().then(games=>{
+        if(games.length == 0){
+            res.send("Não existem jogos registrados ainda")
+        }else{
+            res.send(games)
+            res.statusCode = 200
+        }
+    })
+})
+
+router.post("/game",(req,res)=>{
+    let {title,year,price} = req.body
+    if(title == undefined || isNaN(year) || isNaN(price)){
+        res.sendStatus(400)
+    }else{
+        Game.create({
+            title:title,
+            year:year,
+            price:price
+        }).then(()=>{
+            res.sendStatus(200)
+        }).catch(()=>{
+            res.sendStatus(404)
+        })
+    }
+})
 
 module.exports = router
--- CriandoApi-Rest/index.js
+++ CriandoApi-Rest/index.js
@@ -3,12 +3,14 @@ const app = express()
 const connection = require("./database/database")
 connection.authenticate().then(()=>{console.log("Banco de dados conctado com sucesso")}).catch(erro=>{console.log(`Ocorreu um erro: ${erro}`)})
 const Game = require('./GameController/Game')
+const gameController = require("./GameController/gameController")
 const bodyParser = require("body-parser")
 const PORTA = 8080
 
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
 
+app.use("/",gameController)
 
 app.listen(PORTA, () => {
     console.log(`API RODANDO`)
