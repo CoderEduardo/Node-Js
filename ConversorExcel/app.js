39d5fed427e0a3d12359a88bb4d5c2e09de673a0
Luis Eduardo Ferreira de Faria <luiseduardo250305@gmail.com>
2023-09-18 11:49:53 -0300

    Exibindo as tabelas em um html

--- ConversorExcel/app.js
+++ ConversorExcel/app.js
@@ -2,12 +2,14 @@ const Reader = require("./Reader")
 const leitor = new Reader()
 const Processor = require("./Processor")
 const Table = require("./Table")
+const HtmlParser = require("./HtmlParser")
 
 async function main(){
      let dados = await leitor.Read("./teste.csv")
      let dadosProcessados = Processor.Process(dados)
      let usuarios = new Table(dadosProcessados)
-     console.log(usuarios.RowCount)
+     let html = await HtmlParser.Parse(usuarios)
+     console.log(html)
 }
 
 main()
