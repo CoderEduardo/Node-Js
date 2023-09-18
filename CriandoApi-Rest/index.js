249be5921c3ec3d655fa7bec296ede633ceb80a4
Luis Eduardo Ferreira de Faria <luiseduardo250305@gmail.com>
2023-09-18 16:39:31 -0300

    Criando o model de games

--- CriandoApi-Rest/index.js
+++ CriandoApi-Rest/index.js
@@ -2,6 +2,7 @@ const express = require("express")
 const app = express()
 const connection = require("./database/database")
 connection.authenticate().then(()=>{console.log("Banco de dados conctado com sucesso")}).catch(erro=>{console.log(`Ocorreu um erro: ${erro}`)})
+const Game = require('./GameController/Game')
 const bodyParser = require("body-parser")
 const PORTA = 8080
 
