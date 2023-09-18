648b4f7f98a78a4a58aaf9dbf1479df95402f813
Luis Eduardo Ferreira de Faria <luiseduardo250305@gmail.com>
2023-09-18 16:14:17 -0300

    Atualizando os dados

--- CriandoApi-Rest/index.js
+++ CriandoApi-Rest/index.js
@@ -52,7 +52,7 @@ app.get("/game/:id", (req, res) => {
 
 app.post("/game", (req, res) => {
     let { title, price, year, id } = req.body
-    if (title == undefined || isNaN(price) || isNaN(year) || isNaN(id) ) {
+    if (title == undefined || isNaN(price) || isNaN(year) || isNaN(id)) {
         res.sendStatus(400)
     } else {
         db.games.push({
@@ -66,17 +66,47 @@ app.post("/game", (req, res) => {
     }
 })
 
-app.delete("/game/:id",(req,res)=>{
-    let id = parseInt(req.params.id )
-    if(isNaN(id)){
+app.delete("/game/:id", (req, res) => {
+    let id = parseInt(req.params.id)
+    if (isNaN(id)) {
         res.sendStatus(404)
-    }else{
-        let index = db.games.findIndex(game=>game.id==id)
-        if(index == -1){        //Se o valor retornado for -1 é porque esse elemento não existe
+    } else {
+        let index = db.games.findIndex(game => game.id == id)
+        if (index == -1) {        //Se o valor retornado for -1 é porque esse elemento não existe
             res.sendStatus(404)
-        }else{
-            db.games.splice(index,1)    //Deleta o elemento a partir de uma quantidade indicada
-            res.sendStatus(200) 
+        } else {
+            db.games.splice(index, 1)    //Deleta o elemento a partir de uma quantidade indicada
+            res.sendStatus(200)
+        }
+    }
+})
+
+app.put("/game/:id", (req, res) => {
+    let id = parseInt(req.params.id)
+    if (isNaN(id)) {
+        res.sendStatus(400) //Se o valor que for passador não for um número, ele vai retornar esse status code
+    } else {
+        let game = db.games.find(game => game.id == id)
+        if (game != undefined) {
+
+            let {title,price,year} = req.body
+
+            if (game.title != undefined) {
+                game.title = title
+            }
+
+            if (game.price != undefined) {
+                game.price = price
+            }
+
+            if (game.year != undefined) {
+                game.year = year
+            }
+
+            res.sendStatus(200)
+
+        } else {
+            res.sendStatus(404)
         }
     }
 })
