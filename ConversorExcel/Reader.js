cb24e7d6131c7e0a44970e25798b3f45e9e67161
Luis Eduardo Ferreira de Faria <luiseduardo250305@gmail.com>
2023-09-18 10:09:00 -0300

    Processando os dados, e retornando eles em arrys cortados

--- ConversorExcel/Reader.js
+++ ConversorExcel/Reader.js
@@ -1,10 +1,9 @@
 const fs = require("fs")
-const util = require("util")
-
-class Reader {
+const util = require("util")    //Nos dá acesso a várias coisas interessantes
+class Reader { 
 
     constructor(){
-        this.reader = util.promisify(fs.readFile)
+        this.reader = util.promisify(fs.readFile)       //promisify transforma uma função antiga em uma promisse, nos dando a capacidade de usar async
     }
 
     async Read(filepath) {
--- ConversorExcel/app.js
+++ ConversorExcel/app.js
@@ -1,9 +1,10 @@
 const Reader = require("./Reader")
 const leitor = new Reader()
+const Processor = require("./Processor")
 
 async function main(){
      let dados = await leitor.Read("./teste.csv")
-     console.log(dados)
+     let dadosProcessados = Processor.Process(dados)
 }
 
 main()
