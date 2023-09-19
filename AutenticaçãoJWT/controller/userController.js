a9984c46f5264532a3178adab928cc0e8bf34463
Luis Eduardo Ferreira de Faria <luiseduardo250305@gmail.com>
2023-09-19 18:32:03 -0300

    Criando a autenticação dp usuário

--- AutenticaçãoJWT/controller/userController.js
+++ AutenticaçãoJWT/controller/userController.js
@@ -3,15 +3,28 @@ const router = express.Router()
 const User = require("./User")
 const bcrypt = require("bcryptjs")
 
-/*router.get("/auth", (req, res) => {
+router.get("/auth", (req, res) => {
     let = { email, senha } = req.body
 
     if (email != undefined) {
-
+        User.findOne({
+            email:email
+        }).then(usuario=>{
+            if(usuario!=undefined){
+                let correto = bcrypt.compareSync(senha,usuario.senha)
+                if(correto){
+                    res.sendStatus(200)
+                }else{
+                    res.sendStatus(400)
+                }
+            }else{
+                res.send(404)
+            }
+        })
     } else {
         res.sendStatus(400)
     }
-})*/
+})
 
 router.post("/usuario",(req,res)=>{
     let {nome,email,senha} = req.body
