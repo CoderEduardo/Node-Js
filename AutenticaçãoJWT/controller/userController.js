e667c5ec0d72ac870f30c6435a3c2185487577ca
Luis Eduardo Ferreira de Faria <luiseduardo250305@gmail.com>
2023-09-19 19:32:03 -0300

    Criando a autenticação com o jwt

--- AutenticaçãoJWT/controller/userController.js
+++ AutenticaçãoJWT/controller/userController.js
@@ -7,6 +7,38 @@ const jwt = require("jsonwebtoken")
 //Aqui está declardo a sua senha ultra secreta
 const JWTSecret = "1234"
 
+function auth(req,res,next){
+
+    const authToken = req.headers['authorization']
+
+    if(authToken != undefined){
+
+        const bearer = authToken.split(' ')
+        const token = bearer[1]
+        jwt.verify(token,JWTSecret,(erro,data)=>{
+            if(erro){
+                res.status(401)
+                res.json({erro:"Token inválido"})
+            }else{
+                req.token = token
+                req.loggeUser = {id:data.id,email:data.email}
+                next()
+            }
+        })
+
+    }else{
+        res.status(401)
+        res.json({erro:"Token inválido"})
+    }
+    next()
+
+}
+
+router.get("/games",auth,(req,res)=>{
+    res.statusCode = 200
+    res.send("Olá mundo")
+})
+
 router.get("/auth", (req, res) => {
     let = { email, senha } = req.body
 
@@ -38,7 +70,7 @@ router.get("/auth", (req, res) => {
     }
 })
 
-router.post("/usuario",(req,res)=>{
+router.post("/usuario",auth,(req,res)=>{
     let {nome,email,senha} = req.body
     let salt = bcrypt.genSaltSync(10)
     let hash = bcrypt.hashSync(senha,salt)
