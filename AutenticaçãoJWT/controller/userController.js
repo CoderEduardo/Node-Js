42ed60e78a336ceb45631924ac84328e132b1296
Luis Eduardo Ferreira de Faria <luiseduardo250305@gmail.com>
2023-09-19 19:04:16 -0300

    Criando a sessão com o token jwt

--- AutenticaçãoJWT/controller/userController.js
+++ AutenticaçãoJWT/controller/userController.js
@@ -2,6 +2,10 @@ const express = require("express")
 const router = express.Router()
 const User = require("./User")
 const bcrypt = require("bcryptjs")
+const jwt = require("jsonwebtoken")
+
+//Aqui está declardo a sua senha ultra secreta
+const JWTSecret = "1234"
 
 router.get("/auth", (req, res) => {
     let = { email, senha } = req.body
@@ -13,6 +17,14 @@ router.get("/auth", (req, res) => {
             if(usuario!=undefined){
                 let correto = bcrypt.compareSync(senha,usuario.senha)
                 if(correto){
+                    jwt.sign({id:usuario.id,email:usuario.email},JWTSecret,{expiresIn:"1h"},(erro,token)=>{//Dentro desse método eu consigo passar qualquer valor para o meu token
+                        if(erro){
+                            res.sendStatus(400)
+                        }else{
+                            res.status(200)
+                            res.json({token:token})
+                        }
+                    })   
                     res.sendStatus(200)
                 }else{
                     res.sendStatus(400)
