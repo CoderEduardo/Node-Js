19968749d60d7fff343f345b789200321afe1a55
Luis Eduardo Ferreira de Faria <luiseduardo250305@gmail.com>
2023-09-19 23:00:32 -0300

    Buscando documentos no banco de dados

--- Mongoose/index.js
+++ Mongoose/index.js
@@ -4,34 +4,40 @@ const PORTA = 8080
 const mongoose = require("mongoose")
 
 const linkSchema = new mongoose.Schema({
-    title:{
-        type:String,
-        required:true
+    title: {
+        type: String,
+        required: true
     },
-    description:String,
-    url:{
-        type:String,
-        required:true
+    description: String,
+    url: {
+        type: String,
+        required: true
     },
-    click:{
-        type:Number,
-        default:0
+    click: {
+        type: Number,
+        default: 0
     }
 })
 
-const Link = mongoose.model("Link",linkSchema)
+const Link = mongoose.model("Link", linkSchema)
 
-app.get("/:title",(req,res)=>{
+app.get("/:title", async (req, res) => {
     let title = req.params.title
+    try {
+        let link = await Link.findOne({ title })
+        res.redirect(link.url)
+    } catch (error) {
+        res.send(error)
+    }
 })
 
 
-mongoose.connect('mongodb://127.0.0.1/link').then(()=>{
+mongoose.connect('mongodb://127.0.0.1/link').then(() => {
     console.log("Banco de dados conectado com sucesso")
-}).catch(erro=>{
+}).catch(erro => {
     console.log(`Erro ao conectar: ${erro}`)
 })
 
-app.listen(PORTA,()=>{
+app.listen(PORTA, () => {
     console.log(`O servidor está rodando na porta ${PORTA}`)
 })
