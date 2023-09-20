be2354310f5287f4c51c41f067ee796f92a4d9af
Luis Eduardo Ferreira de Faria <luiseduardo250305@gmail.com>
2023-09-19 22:46:58 -0300

    Criando o model de links

--- Mongoose/index.js
+++ Mongoose/index.js
@@ -3,6 +3,29 @@ const app = express()
 const PORTA = 8080
 const mongoose = require("mongoose")
 
+const linkSchema = new mongoose.Schema({
+    title:{
+        type:String,
+        required:true
+    },
+    description:String,
+    url:{
+        type:String,
+        required:true
+    },
+    click:{
+        type:Number,
+        default:0
+    }
+})
+
+const Link = mongoose.model("Link",linkSchema)
+
+app.get("/:title",(req,res)=>{
+    let title = req.params.title
+})
+
+
 mongoose.connect('mongodb://127.0.0.1/link').then(()=>{
     console.log("Banco de dados conectado com sucesso")
 }).catch(erro=>{
